c628910d7ae8b8325671cc495a1bb3f292b52c65
starik061 <starik061@gmail.com>
2023-04-18 19:21:14 +0300

    Task #1 is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  starBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
+let changeBGColorIntervalId = null;
 
+refs.starBtn.addEventListener('click', () => {
+  changeBodyBGColor();
+  changeBGColorIntervalId = setInterval(changeBodyBGColor, 1000);
+  refs.starBtn.setAttribute('disabled', true);
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(changeBGColorIntervalId);
+  refs.starBtn.removeAttribute('disabled');
+});
+
+function changeBodyBGColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
